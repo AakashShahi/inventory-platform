ebbf6212f0a8a7aa5bbacf147d14ee14e489a398
Aakash Shahi <129369974+AakashShahi@users.noreply.github.com>
2026-09-26 20:07:39 +0545

    build: initialise Spring Boot Maven project (#4)

--- backend-spring/Hello.java
+++
@@ -1,8 +0,0 @@
-public class Hello {
-    public static void main(String[] args) {
-        int stock = 10;
-        int reserved = 3;
-        int available = stock - reserved;
-        System.out.println(available);
-    }
-}
